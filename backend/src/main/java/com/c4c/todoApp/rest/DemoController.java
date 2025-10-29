04f8594bafdd32b26b09bb1253c8872c087d84cb
MUNEHIRO YOSHIDA <yoshidamnhr@gmail.com>
2025-10-29 20:27:50 +0900

    lazy initializationの練習

--- backend/src/main/java/com/c4c/todoApp/rest/DemoController.java
+++ backend/src/main/java/com/c4c/todoApp/rest/DemoController.java
@@ -16,6 +16,7 @@ public class DemoController {
 
     @Autowired
     public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
+        System.out.println("In constructor: " + getClass().getSimpleName());
         myCoach = theCoach;
     }
 
