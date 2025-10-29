7c3d31210ae29861d21871705dd3306e7f694857
MUNEHIRO YOSHIDA <yoshidamnhr@gmail.com>
2025-10-29 21:52:05 +0900

    クラスのライフサイクルについて

--- backend/src/main/java/com/c4c/todoApp/common/CricketCoach.java
+++ backend/src/main/java/com/c4c/todoApp/common/CricketCoach.java
@@ -2,8 +2,27 @@ package com.c4c.todoApp.common;
 
 import org.springframework.stereotype.Component;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 @Component
 public class CricketCoach implements Coach {
+
+    public CricketCoach() {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
+    // define our init method
+    @PostConstruct
+    public void doMyStartupStuff() {
+        System.out.println("CricketCoach: inside method doMyStartupStuff" + getClass().getSimpleName());
+    }
+
+    // define our destroy method
+    @PreDestroy
+    public void doMyCleanupStuff() {
+        System.out.println("CricketCoach: inside method doMyCleanupStuff" + getClass().getSimpleName());
+    }
 
     @Override
     public String getDailyWorkout() {
