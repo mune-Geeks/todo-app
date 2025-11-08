aaa144f4cbd7aeb8e39802f78cb2937630e72f33
MUNEHIRO YOSHIDA <yoshidamnhr@gmail.com>
2025-11-09 03:10:25 +0900

    例外処理の追加

---
+++ backend/src/main/java/com/c4c/todoApp/rest/StudentNotFoundException.java
@@ -0,0 +1,16 @@
+package com.c4c.todoApp.rest;
+
+public class StudentNotFoundException extends RuntimeException {
+
+    public StudentNotFoundException(String message) {
+        super(message);
+    }
+
+    public StudentNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public StudentNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
--- backend/src/main/java/com/c4c/todoApp/rest/StudentRestController.java
+++ backend/src/main/java/com/c4c/todoApp/rest/StudentRestController.java
@@ -3,7 +3,13 @@ package com.c4c.todoApp.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.catalina.connector.Response;
+import org.springframework.boot.autoconfigure.graphql.GraphQlProperties.Http;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,5 +42,55 @@ public class StudentRestController {
 
         return theStudents;
     }
+
+    // define endpoint for "/students/{studentId}" - return student at index
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+
+        // just index into the list ... keep it simple for now
+
+        // check the studentId against list size
+        if ((studentId < 0) || (studentId >= theStudents.size())) {
+            throw new StudentNotFoundException("Student id not found - " + studentId);
+        }
+
+        return theStudents.get(studentId);
+    }
+
+    // add an exception handler using @ExceptionHandler
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
     
+        // create StudentErrorResponse
+
+        StudentErrorResponse error = new StudentErrorResponse();
+
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exc.getMessage());
+        error.setTimestamp(System.currentTimeMillis());
+
+        // return ResponseEntity
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    // add another exception handler ... to catch any exception
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception exc) {
+
+        // create StudentErrorResponse
+
+        StudentErrorResponse error = new StudentErrorResponse();
+
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(exc.getMessage());
+        error.setTimestamp(System.currentTimeMillis());
+
+        // return ResponseEntity
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
 }
