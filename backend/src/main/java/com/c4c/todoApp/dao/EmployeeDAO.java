2d29bf371eb5e14d5ca3349dfacf0f951f96e0cf
MUNEHIRO YOSHIDA <yoshidamnhr@gmail.com>
2025-11-09 23:16:31 +0900

    DAOの実装　登録、更新、削除

--- backend/src/main/java/com/c4c/todoApp/dao/EmployeeDAO.java
+++ backend/src/main/java/com/c4c/todoApp/dao/EmployeeDAO.java
@@ -7,5 +7,10 @@ import com.c4c.todoApp.entity.Employee;
 public interface EmployeeDAO {
 
     List<Employee> findAll();
-    
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
 }
--- backend/src/main/java/com/c4c/todoApp/dao/EmployeeDAOJpaImpl.java
+++ backend/src/main/java/com/c4c/todoApp/dao/EmployeeDAOJpaImpl.java
@@ -36,5 +36,35 @@ public class EmployeeDAOJpaImpl implements EmployeeDAO {
         return employees;
     }
 
+    @Override
+    public Employee findById(int theId) {
+
+        // get the employee
+        Employee theEmployee = entityManager.find(Employee.class, theId);
+
+        // return the employee
+        return theEmployee;
+    }
+
+    @Override
+    public Employee save(Employee theEmployee) {
+
+        // save employee
+        Employee dbEmployee = entityManager.merge(theEmployee);
+
+        // return the saved employee
+        return dbEmployee;
+    }
+
+    @Override
+    public void deleteById(int theId) {
+
+        // find employee by id
+        Employee theEmployee = entityManager.find(Employee.class, theId);
+
+        // remove employee
+        entityManager.remove(theEmployee);
+    }
+
 
 }
