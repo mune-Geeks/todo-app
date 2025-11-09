f93ddb71ca8aa6e072e7b9afe76f3bc14694c0f6
MUNEHIRO YOSHIDA <yoshidamnhr@gmail.com>
2025-11-09 23:26:54 +0900

    Service側の実装　登録、更新、削除

--- backend/src/main/java/com/c4c/todoApp/service/EmployeeService.java
+++ backend/src/main/java/com/c4c/todoApp/service/EmployeeService.java
@@ -7,4 +7,10 @@ import com.c4c.todoApp.entity.Employee;
 public interface EmployeeService {
 
     List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
 }
--- backend/src/main/java/com/c4c/todoApp/service/EmployeeServiceImpl.java
+++ backend/src/main/java/com/c4c/todoApp/service/EmployeeServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.c4c.todoApp.dao.EmployeeDAO;
 import com.c4c.todoApp.entity.Employee;
@@ -22,8 +23,24 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public List<Employee> findAll() {
-        // TODO Auto-generated method stub
         return employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee findById(int theId) {
+        return employeeDAO.findById(theId);
+    }
+
+    @Transactional
+    @Override
+    public Employee save(Employee theEmployee) {
+        return employeeDAO.save(theEmployee);
+    }
+
+    @Transactional
+    @Override
+    public void deleteById(int theId) {
+        employeeDAO.deleteById(theId);
     }
 
 }
