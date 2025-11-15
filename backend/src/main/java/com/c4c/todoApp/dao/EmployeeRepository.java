e6749ebc5c3764737485285762e256e39c9e39cb
MUNEHIRO YOSHIDA <yoshidamnhr@gmail.com>
2025-11-16 04:49:45 +0900

    Spring Data RESTを利用

--- backend/src/main/java/com/c4c/todoApp/dao/EmployeeRepository.java
+++ backend/src/main/java/com/c4c/todoApp/dao/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.c4c.todoApp.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.c4c.todoApp.entity.Employee;
 
+// @RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     // that's it ... no need to write any code !!!
