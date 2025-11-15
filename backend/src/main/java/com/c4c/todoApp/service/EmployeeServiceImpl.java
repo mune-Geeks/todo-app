3e64233a4a4c5c3504ab9084f706e025dddfda36
MUNEHIRO YOSHIDA <yoshidamnhr@gmail.com>
2025-11-16 00:36:24 +0900

    Spring Data JPAを利用したリファクタリング

--- backend/src/main/java/com/c4c/todoApp/service/EmployeeServiceImpl.java
+++ backend/src/main/java/com/c4c/todoApp/service/EmployeeServiceImpl.java
@@ -1,12 +1,12 @@
 package com.c4c.todoApp.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import com.c4c.todoApp.dao.EmployeeDAO;
+import com.c4c.todoApp.dao.EmployeeRepository;
 import com.c4c.todoApp.entity.Employee;
 
 
@@ -14,33 +14,40 @@ import com.c4c.todoApp.entity.Employee;
 public class EmployeeServiceImpl implements EmployeeService {
 
 
-    private EmployeeDAO employeeDAO;
+    private EmployeeRepository employeeRepository;
 
     @Autowired
-    public EmployeeServiceImpl(EmployeeDAO theEmployeeDAO) {
-        this.employeeDAO = theEmployeeDAO;
+    public EmployeeServiceImpl(EmployeeRepository theEmployeeRepository) {
+        employeeRepository = theEmployeeRepository;
     }
 
     @Override
     public List<Employee> findAll() {
-        return employeeDAO.findAll();
+        return employeeRepository.findAll();
     }
 
     @Override
     public Employee findById(int theId) {
-        return employeeDAO.findById(theId);
+        Optional<Employee> result = employeeRepository.findById(theId);
+        Employee theEmployee = null;
+
+        if (result.isPresent()) {
+            theEmployee = result.get();
+        } else {
+            // we didn't find the employee
+            throw new RuntimeException("Did not find employee id - " + theId);
+        }
+        return theEmployee;
     }
 
-    @Transactional
     @Override
     public Employee save(Employee theEmployee) {
-        return employeeDAO.save(theEmployee);
+        return employeeRepository.save(theEmployee);
     }
 
-    @Transactional
     @Override
     public void deleteById(int theId) {
-        employeeDAO.deleteById(theId);
+        employeeRepository.deleteById(theId);
     }
 
 }
