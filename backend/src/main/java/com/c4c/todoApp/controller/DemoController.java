be56eea90e984b68cd8be8dbf9d08bc38baa841a
MUNEHIRO YOSHIDA <yoshidamnhr@gmail.com>
2025-11-16 23:10:17 +0900

    画面に時間を表示する処理

---
+++ backend/src/main/java/com/c4c/todoApp/controller/DemoController.java
@@ -0,0 +1,20 @@
+package com.c4c.todoApp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class DemoController {
+
+    // create a mapping for "/hello"
+    @GetMapping("/hello")
+    public String sayHello(Model themodel) {
+        
+        themodel.addAttribute("theDate", java.time.LocalDateTime.now());
+
+        return "helloworld";
+    }
+    
+}
