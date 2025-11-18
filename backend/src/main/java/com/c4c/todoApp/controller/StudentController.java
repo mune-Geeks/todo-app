e2bca40c86d02ee1f26f672804f934887364b78b
MUNEHIRO YOSHIDA <yoshidamnhr@gmail.com>
2025-11-19 04:22:43 +0900

    Textのform作成

---
+++ backend/src/main/java/com/c4c/todoApp/controller/StudentController.java
@@ -0,0 +1,36 @@
+package com.c4c.todoApp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.c4c.todoApp.model.Student;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@Controller
+public class StudentController {
+
+    @GetMapping("/showStudentForm")
+    public String showForm(Model theModel) {
+
+        // create a student object
+        Student theStudent = new Student();
+
+        // add student object to the model
+        theModel.addAttribute("student", theStudent);
+
+        return "student-form";
+    }
+
+    @PostMapping("/processStudentForm")
+    public String processForm(@ModelAttribute("student") Student theStudent) {
+        // log the input data
+        System.out.println("theStudent: " + theStudent.getFirstName() + " " + theStudent.getLastName());
+
+        return "student-confirmation";
+    }
+
+}
