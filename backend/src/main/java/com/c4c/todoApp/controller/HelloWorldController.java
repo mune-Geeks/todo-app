41602042e914f3f0c0739dc90d8b35313b93e3d8
MUNEHIRO YOSHIDA <yoshidamnhr@gmail.com>
2025-11-19 03:44:35 +0900

    getMappingとpostMappingの違いの確認

--- backend/src/main/java/com/c4c/todoApp/controller/HelloWorldController.java
+++ backend/src/main/java/com/c4c/todoApp/controller/HelloWorldController.java
@@ -2,6 +2,8 @@ package com.c4c.todoApp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -12,7 +14,7 @@ public class HelloWorldController {
 
     // new a controller method to show "Hello, World!" html form
 
-    @RequestMapping("/showForm")
+    @GetMapping("/showForm")
     public String showForm() {
         return "helloworld-form";
     }
@@ -45,7 +47,7 @@ public class HelloWorldController {
         return "helloworld";
     }
 
-    @RequestMapping("/processFormVersionThree")
+    @PostMapping("/processFormVersionThree")
     public String processFormVersionThree(@RequestParam("studentName") String theName, Model model) {
 
         // convert the data to all caps
@@ -59,4 +61,5 @@ public class HelloWorldController {
 
         return "helloworld";
     }
+
 }
