dbca4ddcfbac74231fe079dbcd788ab7b586e03a
MUNEHIRO YOSHIDA <yoshidamnhr@gmail.com>
2025-11-23 00:15:39 +0900

    チェックボックスの実装　プロパティファイルから値を参照

--- backend/src/main/java/com/c4c/todoApp/controller/StudentController.java
+++ backend/src/main/java/com/c4c/todoApp/controller/StudentController.java
@@ -20,6 +20,9 @@ public class StudentController {
     @Value("${languages}")
     private List<String> languages;
 
+    @Value("${systems}")
+    private List<String> systems;
+
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel) {
 
@@ -34,6 +37,9 @@ public class StudentController {
 
         // add languages to the model
         theModel.addAttribute("languages", languages);
+
+        // add systems to the model
+        theModel.addAttribute("systems", systems);
 
         return "student-form";
     }
