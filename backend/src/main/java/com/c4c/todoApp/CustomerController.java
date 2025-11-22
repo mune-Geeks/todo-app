5061dc8865d42b5c0d249554f344ac35df97a4a6
MUNEHIRO YOSHIDA <yoshidamnhr@gmail.com>
2025-11-23 04:55:56 +0900

    initBinderの実装

--- backend/src/main/java/com/c4c/todoApp/CustomerController.java
+++ backend/src/main/java/com/c4c/todoApp/CustomerController.java
@@ -1,9 +1,12 @@
 package com.c4c.todoApp;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -11,6 +14,15 @@ import jakarta.validation.Valid;
 
 @Controller
 public class CustomerController {
+
+    // add an initBinder ... to convert string to uppercase
+    // remove leading and trailing whitespace
+    // resolve issues with validation
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 
     @GetMapping("/")
     public String showForm(Model theModel) {
@@ -22,6 +34,8 @@ public class CustomerController {
 
     @PostMapping("/processForm")
     public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer, BindingResult theBindingResult) {
+
+        System.out.println("Last name: |" + theCustomer.getLastName() + "|");
 
         if (theBindingResult.hasErrors()) {
             return "customer-form";
