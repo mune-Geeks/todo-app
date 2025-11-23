2c3739a0bf423c83477fe6c59056aa0fdea32d52
MUNEHIRO YOSHIDA <yoshidamnhr@gmail.com>
2025-11-23 18:27:12 +0900

    最小値と最大限、パターンマッチングの処理の実装

--- backend/src/main/java/com/c4c/todoApp/Customer.java
+++ backend/src/main/java/com/c4c/todoApp/Customer.java
@@ -1,6 +1,9 @@
 package com.c4c.todoApp;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public class Customer {
@@ -10,6 +13,13 @@ public class Customer {
     @NotNull(message = "is required")
     @Size(min = 1, message = "is required")
     private String lastName = "";
+
+    @Min(value = 0, message = "must be greater than or equal to zero")
+    @Max(value = 10, message = "must be less than or equal to ten")
+    private int freePasses;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "must be a valid zip code")
+    private String postalCode;
 
     public String getFirstName() {
         return firstName;
@@ -25,5 +35,21 @@ public class Customer {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
     }
 }
