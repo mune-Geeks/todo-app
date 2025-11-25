9a4c24a32e8a00285a39a4250e9872286c2dae1e
MUNEHIRO YOSHIDA <yoshidamnhr@gmail.com>
2025-11-26 00:33:59 +0900

    ユーザ権限を使用してページを管理。アクセスできるユーザとできないに分けた

--- backend/src/main/java/com/c4c/todoApp/security/DemoSecurityConfig.java
+++ backend/src/main/java/com/c4c/todoApp/security/DemoSecurityConfig.java
@@ -30,6 +30,9 @@ public class DemoSecurityConfig {
         public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
                 http
                                 .authorizeHttpRequests(configurer -> configurer
+                                                .requestMatchers("/").hasRole("EMPLOYEE")
+                                                .requestMatchers("/leaders/**").hasRole("MANAGER")
+                                                .requestMatchers("/systems/**").hasRole("ADMIN")
                                                 .anyRequest().authenticated())
                                 .formLogin(form -> form
                                                 .loginPage("/showMyLoginPage")
