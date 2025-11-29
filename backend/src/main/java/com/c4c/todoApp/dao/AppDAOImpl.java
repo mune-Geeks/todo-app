420428e89c33668c18b46cc7d8007765e3b9b1c4
MUNEHIRO YOSHIDA <yoshidamnhr@gmail.com>
2025-11-30 03:36:54 +0900

    修正

--- backend/src/main/java/com/c4c/todoApp/dao/AppDAOImpl.java
+++ backend/src/main/java/com/c4c/todoApp/dao/AppDAOImpl.java
@@ -1,12 +1,14 @@
 package com.c4c.todoApp.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.c4c.todoApp.entity.Instructor;
 
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 
+@Repository
 public class AppDAOImpl implements AppDAO {
 
     // define field for entity manager
