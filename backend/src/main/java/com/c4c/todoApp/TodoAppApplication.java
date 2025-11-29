cfdfcf071c30ee45793ffaf6cb42c483e09e2e98
MUNEHIRO YOSHIDA <yoshidamnhr@gmail.com>
2025-11-30 03:41:00 +0900

    インストラクターの追加処理

--- backend/src/main/java/com/c4c/todoApp/TodoAppApplication.java
+++ backend/src/main/java/com/c4c/todoApp/TodoAppApplication.java
@@ -26,12 +26,22 @@ public class TodoAppApplication {
 
 	private void createInstructor(AppDAO appDAO) {
 
+		/*
+		 * // create the instructor
+		 * Instructor tempInstructor = new Instructor("Chad", "Darby",
+		 * "darby@luv2code.com");
+		 * 
+		 * // create the instructorDetail
+		 * InstructorDetail tempInstructorDetail = new InstructorDetail(
+		 * "http://www.luv2code.com/youtube", "Luv 2 Code!");
+		 */
+
 		// create the instructor
-		Instructor tempInstructor = new Instructor("Chad", "Darby", "darby@luv2code.com");
+		Instructor tempInstructor = new Instructor("Madhu", "Patel", "madhu@luv2code.com");
 
 		// create the instructorDetail
 		InstructorDetail tempInstructorDetail = new InstructorDetail(
-				"http://www.luv2code.com/youtube", "Luv 2 Code!");
+				"http://www.luv2code.com/youtube", "Guitar");
 
 		// associate the objects
 		tempInstructor.setInstructorDetail(tempInstructorDetail);
