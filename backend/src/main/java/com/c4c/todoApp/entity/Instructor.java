14d971c6a21f5ea15bd307f0a4be4a8bbda93d0e
MUNEHIRO YOSHIDA <yoshidamnhr@gmail.com>
2025-12-18 05:16:21 +0900

    courseクラス追加の対応。 OneToManyの定義。

--- backend/src/main/java/com/c4c/todoApp/entity/Instructor.java
+++ backend/src/main/java/com/c4c/todoApp/entity/Instructor.java
@@ -1,5 +1,8 @@
 package com.c4c.todoApp.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -7,6 +10,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
@@ -44,6 +48,10 @@ public class Instructor {
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "instructor_detail_id")
     private InstructorDetail instructorDetail;
+
+    @OneToMany(mappedBy = "instructor", cascade = { CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH,
+            CascadeType.DETACH })
+    private List<Course> courses;
 
     public Instructor() {
     }
@@ -100,4 +108,20 @@ public class Instructor {
                 + ", instructorDetail=" + instructorDetail + "]";
     }
 
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
+    // add convenience methods for bi-directional relationship
+    public void add(Course tempCourse) {
+        if (courses == null) {
+            courses = new ArrayList<>();
+        }
+        courses.add(tempCourse);
+        tempCourse.setInstructor(this);
+    }
 }
