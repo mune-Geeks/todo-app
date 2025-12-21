ca386f23f88091cfbf599eb04ecc9f78390ed70a
MUNEHIRO YOSHIDA <yoshidamnhr@gmail.com>
2025-12-22 08:19:19 +0900

    コースとレビューの同時削除処理

--- backend/src/main/java/com/c4c/todoApp/TodoAppApplication.java
+++ backend/src/main/java/com/c4c/todoApp/TodoAppApplication.java
@@ -27,8 +27,21 @@ public class TodoAppApplication {
 
 			// createCourseAndReviews(appDAO);
 
-			retrieveCourseAndReviews(appDAO);
+			// retrieveCourseAndReviews(appDAO);
+
+			deleteCourseAndReviews(appDAO);
 		};
+	}
+
+	private void deleteCourseAndReviews(AppDAO appDAO) {
+
+		int theId = 10;
+
+		System.out.println("Deleting course id: " + theId);
+
+		appDAO.deleteCourseById(theId);
+
+		System.out.println("Done!");
 	}
 
 	private void retrieveCourseAndReviews(AppDAO appDAO) {
