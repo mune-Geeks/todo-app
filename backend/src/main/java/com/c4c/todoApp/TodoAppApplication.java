25b5be3f96536a9b33af9ee17f2be17985a9df69
MUNEHIRO YOSHIDA <yoshidamnhr@gmail.com>
2025-12-27 06:15:54 +0900

    AOP用に環境整備

--- backend/src/main/java/com/c4c/todoApp/TodoAppApplication.java
+++ backend/src/main/java/com/c4c/todoApp/TodoAppApplication.java
@@ -1,7 +1,9 @@
 package com.c4c.todoApp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TodoAppApplication {
@@ -10,4 +12,10 @@ public class TodoAppApplication {
 		SpringApplication.run(TodoAppApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			IO.println("Hello World");
+		};
+	}
 }
